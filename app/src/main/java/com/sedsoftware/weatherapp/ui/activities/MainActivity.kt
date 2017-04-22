da53ec4201e5b68be3ac4cc2c2f58ac7f4029d3c
DJ_Kovrik <djkovrik@gmail.com>
2017-04-22 18:54:57 +0300

    Chapter 12: OnItemClickListener

--- app/src/main/java/com/sedsoftware/weatherapp/ui/activities/MainActivity.kt
+++ app/src/main/java/com/sedsoftware/weatherapp/ui/activities/MainActivity.kt
@@ -6,9 +6,11 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.sedsoftware.weatherapp.R
 import com.sedsoftware.weatherapp.domain.commands.RequestForecastCommand
+import com.sedsoftware.weatherapp.domain.model.Forecast
 import com.sedsoftware.weatherapp.ui.adapters.ForecastListAdapter
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
+import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
@@ -24,7 +26,12 @@ class MainActivity : AppCompatActivity() {
         doAsync {
             val result = RequestForecastCommand("94043").execute()
             uiThread {
-                forecastList.adapter = ForecastListAdapter(result)
+                forecastList.adapter = ForecastListAdapter(result,
+                    object: ForecastListAdapter.OnItemClickListener {
+                        override fun invoke(forecast: Forecast) {
+                            toast(forecast.date)
+                        }
+                    })
             }
         }
     }
