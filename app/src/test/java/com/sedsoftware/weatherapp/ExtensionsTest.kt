abe9cab1241b20ad22aa2b6486f1b63f94cb3f36
DJ_Kovrik <djkovrik@gmail.com>
2017-05-08 21:54:36 +0300

    Chapter 27: Testing

---
+++ app/src/test/java/com/sedsoftware/weatherapp/ExtensionsTest.kt
@@ -0,0 +1,16 @@
+package com.sedsoftware.weatherapp
+
+import com.sedsoftware.weatherapp.extensions.toDateString
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.text.DateFormat
+
+class ExtensionsTest {
+  @Test fun testLongToDateString() {
+    assertEquals("Oct 19, 2015", 1445275635000L.toDateString())
+  }
+
+  @Test fun testDateStringFullFormat() {
+    assertEquals("Monday, October 19, 2015", 1445275635000L.toDateString(DateFormat.FULL))
+  }
+}
